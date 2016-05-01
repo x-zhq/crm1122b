f14880f240edfe67278c759d9a1535681ebf50f8
x-zhq <x-zhq@163.com>
2016-05-01 13:21:11 +0800

    Update TestEgit.java

--- src/com/atguigu/egit/TestEgit.java
+++ src/com/atguigu/egit/TestEgit.java
@@ -7,6 +7,8 @@ public class TestEgit {
 		System.out.println("request 02");
 		System.out.println("request 03");
 		System.out.println("response 01");
+		System.out.println("response 02");
+		
 	}
 	
 }
